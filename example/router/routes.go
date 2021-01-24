fa273d2af095378d4bd1d6c0e91c678a0d8873bd
54m <30588003+54m@users.noreply.github.com>
2021-01-24 22:50:31 +0900

    clean: delete unused parameter name

--- example/router/routes.go
+++ example/router/routes.go
@@ -3,16 +3,16 @@ package router
 import "github.com/labstack/echo/v4"
 
 // Index - Example function
-func Index(c echo.Context) error {
+func Index(_ echo.Context) error {
 	return nil
 }
 
 // Check - Example function
-func Check(c echo.Context) error {
+func Check(_ echo.Context) error {
 	return nil
 }
 
 // Ping - Example function
-func Ping(c echo.Context) error {
+func Ping(_ echo.Context) error {
 	return nil
 }
